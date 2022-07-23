fa341995157cf537a625c2360e51bd857935cda9
takahiro.tominaga <tt.300607@gmail.com>
2022-07-23 11:52:00 +0900

    use repo info

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@actions/core");
+const github_1 = require("@actions/github");
 const inputName = (0, core_1.getInput)("name");
-greet(inputName);
-function greet(name) {
-    console.log(`'Hello ${name}'`);
+greet(inputName, getRepoUrl(github_1.context));
+function greet(name, repoUrl) {
+    console.log(`'Hello ${name}. You are running in ${repoUrl}'`);
+}
+function getRepoUrl({ repo, serverUrl }) {
+    return `${serverUrl}/${repo.owner}/${repo.repo}`;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import { getInput } from "@actions/core";
+import { context } from "@actions/github";
+
+type GithubContext = typeof context;
 
 const inputName = getInput("name");
-greet(inputName);
+greet(inputName, getRepoUrl(context));
 
-function greet(name: string) {
-  console.log(`'Hello ${name}'`);
+function greet(name: string, repoUrl: String) {
+  console.log(`'Hello ${name}. You are running in ${repoUrl}'`);
+}
+
+function getRepoUrl({ repo, serverUrl }: GithubContext): string {
+  return `${serverUrl}/${repo.owner}/${repo.repo}`;
 }
